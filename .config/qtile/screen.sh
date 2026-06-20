5b8f8e5b6c27ea1a4cf78e17fb28874b434f3908
Gustavo Lozada <gu.lozada9@gmail.com>
2026-06-20 19:06:24 -0400

    Fix screen.sh script

--- .config/qtile/screen.sh
+++ .config/qtile/screen.sh
@@ -1,27 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SCREEN_NAME=${1:-"eDP-1"}
 HDMI_NAME=${2:-"HDMI-1"}
-HDMI2_NAME=${2:-"HDMI-2"}
+HDMI2_NAME=${3:-"HDMI-2"}
 
-echo $SCREEN_NAME
-echo $HDMI_NAME
+is_connected() {
+    xrandr --query | awk -v output="$1" '$1 == output && $2 == "connected" { found = 1 } END { exit !found }'
+}
 
-if [[ -n `xrandr --query | grep "$HDMI_NAME connected"` ]]
-then
-    # xrandr --output $HDMI_NAME --mode 1920x1080 --right-of $SCREEN_NAME
-    xrandr --output $SCREEN_NAME --gamma 1:1:1
-    xrandr --output $HDMI_NAME --mode 1920x1080 --above $SCREEN_NAME
-    if [[ -n `xrandr --query | grep "$HDMI2_NAME connected"` ]]
-    then
-        xrandr --output $HDMI2_NAME --mode 1920x1080 --right-of $SCREEN_NAME --right-of $HDMI_NAME --rotate left
-    fi
+if is_connected "$SCREEN_NAME"; then
+    xrandr --output "$SCREEN_NAME" --auto --gamma 1:1:1
+fi
+
+if is_connected "$HDMI_NAME" && is_connected "$HDMI2_NAME"; then
+    xrandr --output "$HDMI_NAME" --auto --above "$SCREEN_NAME"
+    xrandr --output "$HDMI2_NAME" --auto --right-of "$HDMI_NAME" --rotate left
+    setxkbmap -option compose:ralt
+elif is_connected "$HDMI_NAME"; then
+    xrandr --output "$HDMI_NAME" --auto --above "$SCREEN_NAME"
+    xrandr --output "$HDMI2_NAME" --off
+    setxkbmap -option compose:ralt
+elif is_connected "$HDMI2_NAME"; then
+    xrandr --output "$HDMI_NAME" --off
+    xrandr --output "$HDMI2_NAME" --auto --above "$SCREEN_NAME"
     setxkbmap -option compose:ralt
 else
-    xrandr --output $HDMI_NAME --off
-    xrandr --output $HDMI2_NAME --off
-    xrandr --output $SCREEN_NAME --gamma 1:1:1
-    setxkbmap -option compose:ctrl
+    xrandr --output "$HDMI_NAME" --off
+    xrandr --output "$HDMI2_NAME" --off
     setxkbmap -option compose:ralt
 fi
 
